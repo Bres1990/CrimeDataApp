9d671d59726b1ef688a5ecc6fba799880b0efc61
Bres1990 <adam.poteralowicz@gmail.com>
2020-06-12 21:13:37 +0200

    Swapped json file used for Mapbox map source

--- app/src/main/java/com/apap/crimedataapp/map/fragment/CrimeMapFragment.kt
+++ app/src/main/java/com/apap/crimedataapp/map/fragment/CrimeMapFragment.kt
@@ -11,11 +11,8 @@ import com.apap.crimedataapp.map.contract.LocationContract
 import com.apap.crimedataapp.map.presenter.LocationPresenter
 import com.mapbox.mapboxsdk.annotations.PolygonOptions
 import com.mapbox.mapboxsdk.geometry.LatLng
-import com.mapbox.mapboxsdk.geometry.LatLngBounds
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
-import com.mapbox.mapboxsdk.style.layers.FillLayer
-import com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillOpacity
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import kotlinx.android.synthetic.main.crime_map_view.*
 import timber.log.Timber
@@ -25,15 +22,9 @@ import javax.inject.Inject
 
 
 class CrimeMapFragment : BaseMapFragment(), LocationContract.View {
-
-
+    
     @Inject
     protected lateinit var locationPresenter: LocationPresenter
-
-    private val USA_BOUNDS: LatLngBounds = LatLngBounds.Builder()
-            .include(LatLng(49.38, -66.94))
-            .include(LatLng(25.82, -124.39))
-            .build()
 
     lateinit var map: MapboxMap
     private var previouslyClickedRegion: MutableList<LatLng> = mutableListOf()
@@ -51,19 +42,11 @@ class CrimeMapFragment : BaseMapFragment(), LocationContract.View {
             mapboxMap.setStyle(Style.Builder().fromUri("mapbox://styles/bres1990/cjktkm7660lke2sot77i1vbo1"))
             mapboxMap.getStyle {
                 try {
-                    val geoJsonUrl = URI("http://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_500k.json")
-                    val geoJsonSource = GeoJsonSource("us_states", geoJsonUrl)
-                    it.addSource(geoJsonSource)
-
-                    val states = FillLayer("state_contours", "us_states")
-                    states.setProperties(fillOpacity(0.25f))
-                    it.addLayer(states)
+                    it.addSource(GeoJsonSource("world", URI("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json")))
                 } catch (exception: URISyntaxException) {
                     Timber.d(exception)
                 }
             }
-
-            //mapboxMap.setLatLngBoundsForCameraTarget(USA_BOUNDS)
 
             mapboxMap.addOnMapClickListener { point ->
                 locationPresenter.getStateForLocation(point)
@@ -74,7 +57,6 @@ class CrimeMapFragment : BaseMapFragment(), LocationContract.View {
                 val clickedRegion = mapboxMap.queryRenderedFeatures(mapboxMap.projection.toScreenLocation(point), "state_contours")
                 if (clickedRegion.isNotEmpty())
                     locationPresenter.getBoundsForState(clickedRegion[0])
-                else Timber.i("clicked region empty")
                 false;
             }
 
