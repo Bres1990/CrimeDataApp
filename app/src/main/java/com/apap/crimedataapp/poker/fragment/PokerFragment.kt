044a5f01fa5e64d90ae11172ba0cddf86f0c2710
Bres1990 <adam.poteralowicz@gmail.com>
2020-06-17 01:25:43 +0200

    Corrected import in PokerFragment

--- app/src/main/java/com/apap/crimedataapp/poker/fragment/PokerFragment.kt
+++ app/src/main/java/com/apap/crimedataapp/poker/fragment/PokerFragment.kt
@@ -1,13 +1,13 @@
 package com.apap.crimedataapp.poker.fragment
 
+import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.apap.crimedataapp.R
-import com.apap.crimedataapp.base.BaseMapFragment
 
-class PokerFragment : BaseMapFragment() {
+class PokerFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.poker_view, container, false)
