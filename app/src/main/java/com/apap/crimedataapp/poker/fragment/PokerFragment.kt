720abef95e87cf93fe6efcb48a36a94ba2c1fd86
Bres1990 <adam.poteralowicz@gmail.com>
2020-06-17 01:23:40 +0200

    Attached layout to PokerFragment

--- app/src/main/java/com/apap/crimedataapp/poker/fragment/PokerFragment.kt
+++ app/src/main/java/com/apap/crimedataapp/poker/fragment/PokerFragment.kt
@@ -1,7 +1,17 @@
 package com.apap.crimedataapp.poker.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.apap.crimedataapp.R
 import com.apap.crimedataapp.base.BaseMapFragment
 
 class PokerFragment : BaseMapFragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.poker_view, container, false)
+
+        // TODO save chosen state and points in Player class
+    }
 }
